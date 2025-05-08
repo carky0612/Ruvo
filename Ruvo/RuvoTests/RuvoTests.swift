511be0cec668f786b6b8bd551e013b09a1bbb0de
Carly <csmit288@clpoly.edu>
2025-05-07 18:40:13 -0700

    added file

---
+++ Ruvo/RuvoTests/RuvoTests.swift
@@ -0,0 +1,17 @@
+//
+//  RuvoTests.swift
+//  RuvoTests
+//
+//  Created by Carly Smith on 4/15/25.
+//
+
+import Testing
+@testable import Ruvo
+
+struct RuvoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
